1537e61db12029236ba65ca1bd10e2b36a188bde
sebcamp11 <105763585+gxboy11@users.noreply.github.com>
2023-10-25 20:46:37 -0600

    Revert "Merge pull request #1 from gxboy11/Joseph"
    This reverts commit 566df51de8f074a535ce2ca2215cab971e56eb34, reversing
    changes made to 53deb3801a41571a5573b23c3e6ce12f536d1c1c.

--- Assets/Scripts/SpaceShipController.cs
+++ Assets/Scripts/SpaceShipController.cs
@@ -1,91 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SpaceshipController : MonoBehaviour
+public class SpaceShipController : MonoBehaviour
 {
+
     [Header("Movement")]
+    [SerializeField]
     [Tooltip("Z Axis")]
-    [SerializeField] float forwardSpeed = 30.0F;
+    float forwardSpeed = 30.0f;
+
+    [SerializeField]
     [Tooltip("X Axis")]
-    [SerializeField] float strafeSpeed = 8.0F;
+    float strafeSpeed = 8.0f; // Cuando nos movemos a la derecha o la izquierda
+
+    [SerializeField]
     [Tooltip("Y Axis")]
-    [SerializeField] float hoverSpeed = 5.0F;
+    float hoverSpeed = 3.5f; //Movimiento con la nariz de la nave (arriba o abajo)
 
+    [Header("Acceleration")] //Cuanto duramos de 0-1
+    [SerializeField]
+    float forwardAcceleration = 2.5f;
 
+    [SerializeField]
+    float strafeAcceleration = 2.0f;
 
-    [Header("Acceleration")]
-    [SerializeField] float forwardAcceleration = 2.5F;
-    [SerializeField] float strafeAcceleration = 2.0F;
-    [SerializeField] float hoverAcceleration = 2.0F;
+    [SerializeField]
+    float hoverAcceleration = 2.0f;
 
     [SerializeField]
     float minAcceleration = 1.0f;
 
     [Header("Roll")]
-    [SerializeField] float rollSpeed = 85.0F;
-    [SerializeField] float rollAcceleration = 1.5F;
+    [SerializeField]
+    float rollSpeed = 85.0f;
+
+    [SerializeField]
+    float rollAcceleration = 3.5f;
 
     Rigidbody _rb;
 
     float _activeForwardSpeed;
     float _activeStrafeSpeed;
     float _activeHoverSpeed;
-    float _lookRateSpeed = 75.0F;
-    float _rollInput;
 
+    float _lookRateSpeed = 75.0f;
+
+    float _rollInput;
 
 
     Vector2 _lookInput;
     Vector2 _screenCenter;
     Vector2 _mouseDistance;
 
-    void Awake()
+
+    private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
     }
 
-    void Start()
+    private void Start()
     {
-        _screenCenter = new Vector2(Screen.width * 0.5F, Screen.height * 0.5F);
+        _screenCenter = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
     }
 
 
     void Update()
     {
         HandleInputs();
-
     }
+
     void FixedUpdate()
     {
-    
-            if (!isMoving())
+
+        if (!isMoving())
         {
             _rb.position += transform.forward * minAcceleration * Time.fixedDeltaTime;
         }
 
-        transform.Rotate(-_mouseDistance.y * Time.fixedDeltaTime * _lookRateSpeed,
-                          _mouseDistance.x * Time.fixedDeltaTime * _lookRateSpeed,
-                          _rollInput * Time.fixedDeltaTime * rollSpeed,
-                          Space.Self);
-                          
-        _rb.position += transform.forward * _activeForwardSpeed * Time.fixedDeltaTime;
+        transform.Rotate(-_mouseDistance.y * _lookRateSpeed * Time.fixedDeltaTime,
+            _mouseDistance.x * _lookRateSpeed * Time.fixedDeltaTime,
+            _rollInput * rollSpeed * Time.fixedDeltaTime,
+            Space.Self);
+
+        _rb.position += transform.forward * _activeForwardSpeed * Time.fixedDeltaTime; //FixedDeltaTime porque estamos haciendolo en el FixedUpdate
         _rb.position += transform.right * _activeStrafeSpeed * Time.fixedDeltaTime;
         _rb.position += transform.up * _activeHoverSpeed * Time.fixedDeltaTime;
-
     }
+
     void HandleInputs()
     {
         _lookInput = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         _mouseDistance = new Vector2((_lookInput.x - _screenCenter.x) / _screenCenter.x,
-                                     (_lookInput.y - _screenCenter.y) / _screenCenter.y);
+            (_lookInput.y - _screenCenter.y) / _screenCenter.y);
 
-        _mouseDistance = Vector2.ClampMagnitude(_mouseDistance, 1.0F);
+        _mouseDistance = Vector2.ClampMagnitude(_mouseDistance, 1.0f); //si la magnitud de este vector es mayor a 1, entonces devuelvame un vector cuya magnitud sea 1
 
         _rollInput = Mathf.Lerp(_rollInput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
-
-
 
         float currentForwardSpeed = Input.GetAxisRaw("Forward") * forwardSpeed;
         _activeForwardSpeed = Mathf.Lerp(_activeForwardSpeed, currentForwardSpeed, forwardAcceleration * Time.deltaTime);
@@ -93,51 +104,8 @@ public class SpaceshipController : MonoBehaviour
         float currentStrafeSpeed = Input.GetAxisRaw("Horizontal") * strafeSpeed;
         _activeStrafeSpeed = Mathf.Lerp(_activeStrafeSpeed, currentStrafeSpeed, strafeAcceleration * Time.deltaTime);
 
-        float currentHoverSpeed = Input.GetAxisRaw("Hover") * hoverSpeed;
+        float currentHoverSpeed = Input.GetAxisRaw("Hover") * hoverSpeed; //Edit / Project Settings / Input
         _activeHoverSpeed = Mathf.Lerp(_activeHoverSpeed, currentHoverSpeed, hoverAcceleration * Time.deltaTime);
-
-
-
-
-
-
-        //_activeForwardSpeed = Input.GetAxisRaw("Vertical") * forwardSpeed;
-        //_activeStrafeSpeed = Input.GetAxisRaw("Horizontal") * strafeSpeed;
-        //_activeHoverSpeed = Input.GetAxisRaw("Hover") * hoverSpeed;
-
-
-
-    }
-
-
-
-
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Colisi�n detectada con: " + collision.gameObject.tag);
-        if (collision.gameObject.CompareTag("Asteroid") || collision.gameObject.CompareTag("Destructor"))
-        {
-            // Se reinicia el juego
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-    }
-
-
-
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Gatillo ingresado con: " + other.gameObject.tag);
-
-        if (other.gameObject.CompareTag("Asteroid") || other.gameObject.CompareTag("Destructor"))
-        {
-            Debug.Log("Tocado: " + other.gameObject.tag);
-            // Se reinicia el juego
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-
-
     }
 
     bool isMoving()
@@ -149,4 +117,3 @@ public class SpaceshipController : MonoBehaviour
         return true;
     }
 }
-
